95ba75b6884e0b4fbaa5c1109a0ed42103e9eb44
Alfio <tomarchioalfio5@gmail.com>
2024-01-23 14:04:08 +0000

    Add support for implicit multiplication by absolute value groupings
    This comes with the limitation of disallowing immediately nested absolute value implicit multiplication.

--- src/parser.ts
+++ src/parser.ts
@@ -2,9 +2,9 @@ import { Token, TokenType } from './token';
 import * as AST from './ast';
 
 /*
-    Grammar defined like so:
+    Ideal math grammar defined like so:
 
-    declaration -> IDENTIFIER "(" IDENTIFIER ("," IDENTIFIER)* ")" "=" expression
+    fnDeclaration -> IDENTIFIER "(" IDENTIFIER ("," IDENTIFIER)* ")" "=" expression
 
      expression -> term
            term -> factor (("+" | "-") factor)*
@@ -19,12 +19,27 @@ import * as AST from './ast';
     absGrouping -> "|" expression "|"
 
              fn -> RESERVED_FUNCTION "(" expression (',' expression)* ")"
+
+
+    Verbena's grammar only differs in that it doesn't allow immediately nested absolute value implicit multiplication.
+    Expressions like this are disallowed, and * signs must be inserted:
+    |2|3||
+    ||2|3|
+
 */
 
 export function parse(tokenStream: Token[]) {
     let currentToken = 0;
 
     // HELPERS
+
+    /*
+    Helper stack for absolute value. 
+    New values are pushed when depth increases,
+    and set to true when the parser is in an AbsGropuing 
+    at the current depth. 
+    */
+    let absStack = [false];
 
     //get current token without advancing
     let peek = () => tokenStream[currentToken];
@@ -50,7 +65,7 @@ export function parse(tokenStream: Token[]) {
     //conditional advance, else error
     let expect = (type: TokenType, message: string) => {
         if (check(type)) return advance();
-        throw Error('(' + peek().lexeme + ') ' + message);
+        throw Error('[' + peek().lexeme + '] ' + message);
     }
 
     // GRAMMAR IMPLEMENTATION
@@ -62,10 +77,9 @@ export function parse(tokenStream: Token[]) {
 
         let params: Token[] = [];
         if (!check(TokenType.PAREN_R)) {
-            params.push(expect(TokenType.IDENTIFIER, 'Expected function parameter name or closing parenthesis'));
-            while (match(TokenType.COMMA)) {
-                params.push(expect(TokenType.IDENTIFIER, 'Expected function parameter name after comma'));
-            }
+            do {
+                params.push(expect(TokenType.IDENTIFIER, 'Expected function parameter name or closing parenthesis'));
+            } while (match(TokenType.COMMA));
         };
 
         expect(TokenType.PAREN_R, 'Expected \')\' after function parameter list');
@@ -95,22 +109,27 @@ export function parse(tokenStream: Token[]) {
     function factor() {
         let left = exponent();
 
-
         //match for explicit multiplication/division tokens and implicit multiplication by primary types
-        while (match(TokenType.STAR, TokenType.SLASH, TokenType.NUMBER, TokenType.IDENTIFIER, TokenType.CONSTANT, TokenType.PAREN_L, /*TokenType.PIPE,*/ TokenType.FUNCTION)) {
+        //only match for PIPE if we are not in an abs at the current depth: match for star again otherwise (redundant)
+        let matchForPipeToken = (absStack[absStack.length - 1] ? TokenType.STAR : TokenType.PIPE);
+
+        while (match(TokenType.STAR, TokenType.SLASH, TokenType.NUMBER, TokenType.IDENTIFIER, TokenType.CONSTANT, TokenType.PAREN_L, TokenType.FUNCTION, matchForPipeToken)) {
             let token = previous();
             switch (token.type) {
                 case TokenType.STAR:
                 case TokenType.SLASH:
+                    absStack.push(false);
                     left = new AST.Binary(left, token, exponent());
+                    absStack.pop();
                     break;
+                case TokenType.PIPE:
                 case TokenType.NUMBER:
                 case TokenType.IDENTIFIER:
                 case TokenType.CONSTANT:
                 case TokenType.PAREN_L:
-                //case TokenType.PIPE:
                 case TokenType.FUNCTION:
                     currentToken--;
+                    if (absStack[absStack.length - 1] && (previous().type == TokenType.PIPE)) throw new Error("Implicit multiplication with nested absolute value is not allowed, try inserting a * symbol");
                     left = new AST.Binary(left, new Token(TokenType.STAR, '*'), exponent());
                     break;
             }
@@ -122,8 +141,10 @@ export function parse(tokenStream: Token[]) {
     function exponent() {
         let left = unary();
         if (match(TokenType.CARAT)) {
+            absStack.push(false);
             let operator = previous();
             let right = exponent();
+            absStack.pop();
             left = new AST.Binary(left, operator, right);
         }
         return left;
@@ -144,22 +165,44 @@ export function parse(tokenStream: Token[]) {
             return new AST.Literal(previous());
         }
         if (match(TokenType.PAREN_L)) {
+            absStack.push(false);
             let inner = expression();
+            absStack.pop();
             expect(TokenType.PAREN_R, 'unclosed grouping');
             return new AST.Grouping(inner);
         }
         if (match(TokenType.PIPE)) {
-            let inner = expression();
-            expect(TokenType.PIPE, 'unclosed grouping');
-            return new AST.AbsGrouping(inner);
+            //only open a new abs grouping if we're not in an abs grouping
+            if (!absStack[absStack.length - 1]) {
+                absStack[absStack.length - 1] = true;
+                let inner = expression();
+                expect(TokenType.PIPE, 'unclosed grouping');
+                absStack[absStack.length - 1] = false;
+                return new AST.AbsGrouping(inner);
+            }
+            //otherwise, if the PIPE is preceded by another PIPE or +/-, it's a valid nesting
+            switch (tokenStream[currentToken - 2].type) {
+                case TokenType.PIPE:
+                case TokenType.PLUS:
+                case TokenType.MINUS:
+                    absStack.push(true);
+                    let inner = expression();
+                    expect(TokenType.PIPE, 'unclosed grouping');
+                    absStack.pop();
+                    return new AST.AbsGrouping(inner);
+            }
+            throw new Error(`[${currentToken}] Unexpected abs bar`);
         }
         if (match(TokenType.FUNCTION)) {
             let ident = previous();
             let args: AST.Expr[] = [];
             expect(TokenType.PAREN_L, 'expected \'(\' after function');
             if (!check(TokenType.PAREN_R)) {
-                args.push(expression());
-                while (match(TokenType.COMMA)) args.push(expression());
+                do {
+                    absStack.push(false);
+                    args.push(expression());
+                    absStack.pop();
+                } while (match(TokenType.COMMA));
             }
             expect(TokenType.PAREN_R, 'expected \')\' after function arguments');
 
